69935054035472628331cf2004e0bf8e888bfe57
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-09 22:59:23 -0300

    Update in API

--- API.js
+++ API.js
@@ -1,12 +1,6 @@
 import express, { json } from 'express'
 const app = express();
 
-
-//Filtrar por categoria: Endpoint que permite filtrar os livros por categoria específica, como romance, aventura, suspense, etc.
-//Listar todos os livros: Endpoint para obter uma lista completa de todos os livros disponíveis.
-//Pesquisa por título ou autor: Endpoint para buscar livros que correspondam a uma consulta de pesquisa baseada no título do livro ou no nome do autor.
-//Buscar por ID: Endpoint para recuperar detalhes de um livro específico com base no ID.
-
 const livros = [{
     id: 1,
     titulo: 'Orgulho e Preconceito',
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # API.library
 API for library
+
+Requisitos: 
+
+
+# Filtrar por categoria: Endpoint que permite filtrar os livros por categoria específica, como romance, aventura, suspense, etc.
+
+# Listar todos os livros: Endpoint para obter uma lista completa de todos os livros disponíveis.
+
+# Pesquisa por título ou autor: Endpoint para buscar livros que correspondam a uma consulta de pesquisa baseada no título do livro ou no nome do autor.
+
+# Buscar por ID: Endpoint para recuperar detalhes de um livro específico com base no ID.
+
