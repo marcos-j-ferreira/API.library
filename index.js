fb66f8e9544c23c4358cae5a7a04b00e1e2e7489
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-13 16:45:06 -0300

    update in index

--- index.js
+++ index.js
@@ -108,18 +108,9 @@ class library {
     }
   }
 }
-const aa = new library()
-aa.filtrar('Aventura') // Procurar por categorias
-aa.allbook()  // listar todos os livros 
-aa.busqueBook("As Crônicas de Nárnia") //// buscar autor ou livro
-aa.busqueID(6) // Busca todas as informações do livro
+const functionINlibrary = new library()
 
-
-/*
-Filtrar por categoria: Endpoint que permite filtrar os livros por categoria específica,
-como romance, aventura, suspense, etc.
-Listar todos os livros: Endpoint para obter uma lista completa de todos os livros disponíveis.
-Pesquisa por título ou autor: Endpoint para buscar livros que correspondam a uma 
-consulta de pesquisa baseada no título do livro ou no nome do autor.
-Buscar por ID: Endpoint para recuperar detalhes de um livro específico com base no ID.
-*/
+functionINlibrary.filtrar('Aventura') // Procurar por categorias
+functionINlibrary.allbook()  // listar todos os livros 
+functionINlibrary.busqueBook("As Crônicas de Nárnia") //// buscar autor ou livro
+functionINlibrary.busqueID(6) // Busca todas as informações do livro
